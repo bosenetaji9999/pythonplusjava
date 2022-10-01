19f36da306643a39d9bd40be0848d6b05db00373
MehtaHarshad <72484481+MehtaHarshad@users.noreply.github.com>
2022-10-01 11:36:28 +0530

    requestion raised draft

--- test/run.sh
+++ test/run.sh
@@ -12,6 +12,16 @@ exit $ret
 fi
 rm -rf target
 
+if [ $ret -ne 0 ]; then
+exit $
+if [ $ret -ne 0 ]; then
+exit $ret
+fi
+rm -rf target
+
+./gradlew build
+ret=$?
+
 ./gradlew build
 ret=$?
 if [ $ret -ne 0 ]; then
